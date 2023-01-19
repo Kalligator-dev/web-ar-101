b4d8ed962462be00e100e38352d029b56f00cff5
Abdullah <kalli@kalligator.com>
2023-01-19 16:24:16 +0200

    v0.3 added sounds that change with different animations ... also added the ability to change animations by tapping the 3d model itself ... minor css refactoring to change container active/deact state by css

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,14 @@ import bezier from "bezier-easing";
 import { MindARThree } from "mind-ar/dist/mindar-image-three.prod";
 import { mockWithImage } from "./camera-mock";
 // import { mockWithVideo } from "./helpers";
-import { loadGLTF } from "./loader";
+import { loadGLTF, loadAudio } from "./loader";
 
 const container = document.querySelector("#container");
 const mindarThree = new MindARThree({
   container,
   imageTargetSrc: "./assets/azeria.mind",
+  // filterMinCF: 0.0001,
+  filterBeta: 100000,
 });
 const { renderer, scene, camera } = mindarThree;
 const anchor = mindarThree.addAnchor(0);
@@ -20,6 +22,24 @@ gltf.scene.scale.set(0.0065, 0.0065, 0.0065);
 gltf.scene.position.set(-0.05, -0.04, 0);
 anchor.group.add(gltf.scene);
 
+const audioClip = await loadAudio("assets/sound/cuckoo.mp3");
+const audioClip1 = await loadAudio("assets/sound/dog-walking.wav");
+const audioClip2 = await loadAudio("assets/sound/dog-running.wav");
+const audioClips = [audioClip, audioClip1, audioClip2];
+const listener = new THREE.AudioListener();
+const audioIdle = new THREE.PositionalAudio(listener);
+const audioWalk = new THREE.PositionalAudio(listener);
+const audioRun = new THREE.PositionalAudio(listener);
+const audios = [audioIdle, audioWalk, audioRun];
+camera.add(listener);
+audios.forEach((audio, i) => {
+  audio.setRefDistance(5);
+  audio.setBuffer(audioClips[i]);
+  audio.setLoop(true);
+  audio.setVolume(200);
+  anchor.group.add(audio);
+});
+
 const mixer = new THREE.AnimationMixer(gltf.scene);
 const animations = new Array(gltf.animations.length)
   .fill(null)
@@ -27,6 +47,14 @@ const animations = new Array(gltf.animations.length)
 let currentAnimation = 0;
 let animationDirection = -1;
 animations[0].play();
+anchor.onTargetFound = () => {
+  audios[0].play();
+  if (currentAnimation !== 0) audios[currentAnimation].play();
+};
+anchor.onTargetLost = () => {
+  audios[0].pause();
+  if (currentAnimation !== 0) audios[currentAnimation].pause();
+};
 
 const fullMotionAngle = 1.6;
 const clock = new THREE.Clock();
@@ -36,7 +64,7 @@ let elapsed = 0;
 let rotationAngle = 0.8;
 
 const start = async () => {
-  // await mockWithVideo("assets/video/mock3.mp4");
+  // await mockWithVideo("assets/video/azeria.mp4");
   await mockWithImage("assets/img/azeria.jpg");
   await mindarThree.start();
   renderer.setAnimationLoop(() => {
@@ -57,6 +85,8 @@ const start = async () => {
     renderer.render(scene, camera);
   });
 };
+
+let stateOfApp = 0;
 const startButton = document.querySelector("#startButton");
 const stopButton = document.querySelector("#stopButton");
 const animButton = document.querySelector("#animButton");
@@ -65,21 +95,43 @@ startButton.addEventListener("click", () => {
   startButton.className = "active";
   stopButton.className = "";
   animButton.className = "";
-  container.style.backgroundColor = "transparent";
+  container.classList.remove("deact");
+  if (stateOfApp > 0) {
+    audios[0].play();
+    if (currentAnimation !== 0) audios[currentAnimation].play();
+  }
   start();
 });
 stopButton.addEventListener("click", () => {
+  stateOfApp++;
+  audios.forEach((audio) => audio.pause());
   startButton.className = "";
   stopButton.className = "deact";
   animButton.className = "deact";
+  container.classList.add("deact");
   mindarThree.stop();
   mindarThree.renderer.setAnimationLoop(null);
   container.style.backgroundColor = "lightgrey";
 });
-animButton.addEventListener("click", () => {
+const rollAnimations = () => {
   animations[currentAnimation].stop();
+  if (currentAnimation !== 0) audios[currentAnimation].pause();
   if (currentAnimation >= gltf.animations.length - 1 || currentAnimation <= 0)
     animationDirection *= -1;
   currentAnimation += animationDirection;
   animations[currentAnimation].play();
+  if (currentAnimation !== 0) audios[currentAnimation].play();
+};
+animButton.addEventListener("click", rollAnimations);
+
+container.addEventListener("click", (e) => {
+  const mouseX = (e.offsetX / container.offsetWidth) * 2 - 1;
+  const mouseY = -1 * ((e.offsetY / container.offsetHeight) * 2 - 1);
+  const mouse = new THREE.Vector2(mouseX, mouseY);
+
+  const raycaster = new THREE.Raycaster();
+  raycaster.setFromCamera(mouse, camera);
+
+  const intersect = raycaster.intersectObject(gltf.scene, true);
+  if (intersect.length) rollAnimations();
 });
